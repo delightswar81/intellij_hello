1cf3ffbc5ee777ef5e62e3926fce5c3ff2b7dade
delightswar81 <73922518+delightswar81@users.noreply.github.com>
2022-08-30 13:11:56 +0900

    GitHub for git pull

--- src/main.java
+++ src/main.java
@@ -8,5 +8,6 @@ public class main {
         System.out.println("Merge complete");
         System.out.println("Hello! Local repository");
         System.out.println("Hello! Github");
+        System.out.println("Hello! git pull);
     }
 }
