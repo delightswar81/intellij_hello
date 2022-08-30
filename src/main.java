68d0f9d151938d2696588ae7beb123f158651833
YOUNGGYU <delightswarm82@mgail.com>
2022-08-30 11:58:37 +0900

    Hello World

--- src/main.java
+++ src/main.java
@@ -2,5 +2,6 @@ public class main {
     public static void main(String[] args){
         // write your code here.
         System.out.println("Hello World");
+        System.out.println("Project Diva");
     }
 }
